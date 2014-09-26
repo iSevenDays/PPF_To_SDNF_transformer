a3adc369c3274351d9db791bf94af53c8dae9d44
xtrafrancyz <xtrafrancyz@mail.com>
2014-09-26 18:38:04 +0300

    Функции СДНФ, СКНФ, исправление багов

--- scripts.js
+++ scripts.js
@@ -4,6 +4,7 @@ var operations = {
 	'!': '!',
 	'(': '(',
 	')': ')',
+	'=': '==',
 };
 
 Array.prototype.contains = function(v) {
@@ -21,6 +22,9 @@ Array.prototype.unique = function() {
     }
     return arr; 
 }
+String.prototype.replaceAll = function(token, newToken){
+    return this.split(token).join(newToken);
+};
 
 function intToBinaryString(num, len){
 	var str = num.toString(2);
@@ -33,13 +37,13 @@ function buildMatrix(str){
 	//Получение всех переменных из выражения
 	var vars = str;
 	for (op in operations)
-		vars = vars.replace(op, '');
-	vars = vars.split("").unique();
+		vars = vars.replaceAll(op, '');
+	vars = vars.toUpperCase().split("").unique();
 	
-	//Построение исполнябельного выражения
+	//Построение выполняемого выражения
 	var expr = str;
 	for (op in operations)
-		expr = expr.replace(op, operations[op]);
+		expr = expr.replaceAll(op, operations[op]);
 	
 	var matrix = [];
 	var size = Math.pow(2, vars.length);
@@ -47,14 +51,59 @@ function buildMatrix(str){
 		var bin = intToBinaryString(i, vars.length);
 		var currExpr = expr;
 		for (var j=0; j<vars.length; j++)
-			currExpr = currExpr.replace(vars[j], (bin.substr(j, 1) == '1').toString());
+			currExpr = currExpr.replaceAll(vars[j], (bin.substr(j, 1) == '1').toString());
 		matrix[bin] = eval(currExpr);
 	}
 	
-	return matrix;
+	return {
+		matrix: matrix,
+		vars: vars.sort()
+	};
 }
 
-var matrix = buildMatrix('A^BvC');
-for (key in matrix)
-	if (typeof(matrix[key]) != "function")
-		console.log(key+' => '+matrix[key]);
+function getSDNF(vars, matrix){
+	var result = '';
+	for (key in matrix){
+		if (matrix[key] === true){
+			result += '(';
+			for (var i=0; i<vars.length; i++){
+				if (i > 0)
+					result += '^';
+				if (key.substr(i, 1) == '0')
+					result += '!';
+				result += vars[i];
+			}
+			result += ')v';
+		}
+	}
+	if (result.length > 0)
+		result = result.substr(0, result.length-1);
+	return result;
+}
+
+function getSKNF(vars, matrix){
+	var result = '';
+	for (key in matrix){
+		if (matrix[key] === false){
+			result += '(';
+			for (var i=0; i<vars.length; i++){
+				if (i > 0)
+					result += 'v';
+				if (key.substr(i, 1) == '1')
+					result += '!';
+				result += vars[i];
+			}
+			result += ')^';
+		}
+	}
+	if (result.length > 0)
+		result = result.substr(0, result.length-1);
+	return result;
+}
+
+var result = buildMatrix('A^BvC');
+for (key in result.matrix)
+	if (typeof(result.matrix[key]) != "function")
+		console.log(key+' => '+result.matrix[key]);
+
+console.log("SDNF: "+getSDNF(result.vars, result.matrix));
